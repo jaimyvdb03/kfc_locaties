257519efa6c955c21c42fa7ba5c810dfceca4e1f
jaimyvdb03 <75025274+jaimyvdb03@users.noreply.github.com>
2025-04-01 15:58:02 +0200

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,157 @@
+const kfcLocations = JSON.parse( [
+        {
+            "naam": "KFC Capelle aan den IJssel",
+            "adres": "Rhijnspoor 300, 2901 LC Capelle aan den IJssel",
+            "telefoon": "010-4472268",
+            "coördinaten": {
+                "latitude": 51.9251993,
+                "longitude": 4.5668268
+            }
+        },
+        {
+            "naam": "KFC Marconiplein",
+            "adres": "Marconiplein 1, 3029 AK Rotterdam",
+            "telefoon": "010-4766960",
+            "coördinaten": {
+                "latitude": 51.912222,
+                "longitude": 4.432083
+            }
+        },
+        {
+            "naam": "KFC Spijkenisse",
+            "adres": "Sportlaan 17, 3202 VW Spijkenisse",
+            "telefoon": "0181-623456",
+            "coördinaten": {
+                "latitude": 51.853533,
+                "longitude": 4.307083
+            }
+        },
+        {
+            "naam": "KFC Wateringen",
+            "adres": "Sylvain Poonsstraat 28, 2292 JN Wateringen",
+            "telefoon": "070-3941246",
+            "coördinaten": {
+                "latitude": 52.0162095,
+                "longitude": 4.3032686
+            }
+        },
+        {
+            "naam": "KFC Groene Hilledijk",
+            "adres": "Groene Hilledijk 154, 3074 AA Rotterdam",
+            "telefoon": "010-4326149",
+            "coördinaten": {
+                "latitude": 51.898,
+                "longitude": 4.511
+            }
+        },
+        {
+            "naam": "KFC Barendrecht",
+            "adres": "Van 't Hofstraat 49, 2991 XP Barendrecht",
+            "telefoon": "010-4838833",
+            "coördinaten": {
+                "latitude": 51.8247601,
+                "longitude": 4.4744454
+            }
+        },
+        {
+            "naam": "KFC Vlaardingen",
+            "adres": "Schiedamsedijk 5, 3134 KK Vlaardingen",
+            "telefoon": "010-4601234",
+            "coördinaten": {
+                "latitude": 51.908283,
+                "longitude": 4.3661
+            }
+        },
+        {
+            "naam": "KFC Cor Kieboomplein",
+            "adres": "Cor Kieboomplein 506, 3077 MK Rotterdam",
+            "telefoon": "010-4790509",
+            "coördinaten": {
+                "latitude": 51.8963551,
+                "longitude": 4.5245454
+            }
+        },
+        {
+            "naam": "KFC Binnenwegplein",
+            "adres": "Binnenwegplein 68, 3012 KL Rotterdam",
+            "telefoon": "010-4131234",
+            "coördinaten": {
+                "latitude": 51.9205,
+                "longitude": 4.4786
+            }
+        },
+        {
+            "naam": "KFC Bergweg",
+            "adres": "Bergweg 234, 3035 BS Rotterdam",
+            "telefoon": "010-4656789",
+            "coördinaten": {
+                "latitude": 51.9321,
+                "longitude": 4.4829
+            }
+        },
+        {
+            "naam": "KFC West-Kruiskade",
+            "adres": "West-Kruiskade 46, 3014 AW Rotterdam",
+            "telefoon": "010-4145678",
+            "coördinaten": {
+                "latitude": 51.9223,
+                "longitude": 4.4697
+            }
+        },
+        {
+            "naam": "KFC Zuidplein Hoog",
+            "adres": "Zuidplein Hoog 534-542, 3083 BL Rotterdam",
+            "telefoon": "010-4856789",
+            "coördinaten": {
+                "latitude": 51.8895,
+                "longitude": 4.4898
+            }
+        },
+        {
+            "naam": "KFC Harnaschpolder",
+            "adres": "Hogenhoorn 7, 2635 BN Den Hoorn",
+            "telefoon": "015-3801234",
+            "coördinaten": {
+                "latitude": 52.0116,
+                "longitude": 4.3211
+            }
+        },
+        {
+            "naam": "KFC Leiden",
+            "adres": "Stationsweg 19, 2312 AT Leiden",
+            "telefoon": "071-5123456",
+            "coördinaten": {
+                "latitude": 52.1663,
+                "longitude": 4.4826
+            }
+        },
+        {
+            "naam": "KFC Den Haag Spui",
+            "adres": "Spui 2, 2511 BD Den Haag",
+            "telefoon": "070-3456789",
+            "coördinaten": {
+                "latitude": 52.0775,
+                "longitude": 4.3131
+            }
+        },
+        {
+            "naam": "KFC Den Haag Leyweg",
+            "adres": "Leyweg 785b, 2545 HA Den Haag",
+            "telefoon": "070-3901234",
+            "coördinaten": {
+                "latitude": 52.0536,
+                "longitude": 4.2897
+            }
+        },
+        {
+            "naam": "KFC Den Haag Hobbemaplein",
+            "adres": "Hobbemaplein 44-45, 2526 JD Den Haag",
+            "telefoon": "070-3567890",
+            "coördinaten": {
+                "latitude": 52.0705,
+                "longitude": 4.3023
+            }
+        }
+]);
+
+export default kfcLocations;
